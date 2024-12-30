103f218ca0d70532798d541d04b0be1ee2d9c7f6
potats0 <bangzhiliang@gmail.com>
2024-12-30 14:00:21 +0800

    perf: 优化url解码

--- src/ngx_vs_modsec_args.c
+++ src/ngx_vs_modsec_args.c
@@ -4,6 +4,42 @@
 
 #include "ngx_vs_modsec_runtime.h"
 
+static const unsigned char hex_table[256] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x00-0x0F */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x10-0x1F */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x20-0x2F */
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, /* 0x30-0x3F */
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x40-0x4F */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x50-0x5F */
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x60-0x6F */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x70-0x7F */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x80-0x8F */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0x90-0x9F */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0xA0-0xAF */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0xB0-0xBF */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0xC0-0xCF */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0xD0-0xDF */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0xE0-0xEF */
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  /* 0xF0-0xFF */
+};
+
+/* 判断是否包含 URL 编码 */
+ngx_flag_t has_url_encoding(u_char *data, size_t len) {
+    if (data == NULL || len == 0) {
+        return 0;
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        if (data[i] == '%') {
+            // 检查是否有合法的两位十六进制字符
+            if (i + 2 < len && hex_table[data[i + 1]] && hex_table[data[i + 2]]) {
+                return 1; // 存在 URL 编码
+            }
+        }
+    }
+    return 0; // 不存在 URL 编码
+}
+
 ngx_array_t *parse_get_args(ngx_str_t *queryString, ngx_pool_t *pool) {
     if (queryString == NULL || queryString->len == 0) {
         MLOGD("No queruy string found");
@@ -62,11 +98,20 @@ ngx_array_t *parse_get_args(ngx_str_t *queryString, ngx_pool_t *pool) {
 
             u_char *dst = arg->decoded.data;
             u_char *src = arg->value.data;
-            ngx_memcpy(dst, src, arg->value.len);
-            ngx_unescape_uri(&dst, &src, arg->value.len, 0);
-            arg->decoded.len = dst - arg->decoded.data;
 
-            MLOGD("GET param: %V = %V decoded: %V", &arg->key, &arg->value, &arg->decoded);
+            if (has_url_encoding(src, arg->value.len)) {
+                // 存在 URL 编码，需要解码
+                ngx_memcpy(dst, src, arg->value.len);
+                ngx_unescape_uri(&dst, &src, arg->value.len, 0);
+                arg->decoded.len = dst - arg->decoded.data;
+                MLOGD("detect url endocoding param: %V = %V decoded: %V (URL encoded)", &arg->key, &arg->value,
+                      &arg->decoded);
+            } else {
+                // 不存在 URL 编码，直接修改指针
+                arg->decoded.data = arg->value.data;
+                arg->decoded.len = arg->value.len;
+                MLOGD("could not detect url encoding param: %V = %V (no URL encoding)", &arg->key, &arg->value);
+            }
         }
     }
 
--- t/sanity/15-rule-http-urldecode.t
+++ t/sanity/15-rule-http-urldecode.t
@@ -101,22 +101,23 @@ GET /test_handler?id=aaa%00aacmd
 Matched pattern: cmd
 --- no_error_log
 [error]
-# === TEST 6: url不存在编码，自动跳过
-# --- http_config
-#     error_log logs/error.log debug;
-# --- config
-#     location /test_handler {
-#         error_log logs/error.log debug;
-#         rule 'rule 1000 http.get_args["id"] contains "cmd"  ;';
-#         proxy_pass http://127.0.0.1:$TEST_NGINX_SERVER_PORT/echo;
-#     }
+=== TEST 6: url不存在编码，自动跳过
+--- http_config
+    error_log logs/error.log debug;
+--- config
+    location /test_handler {
+        error_log logs/error.log debug;
+        rule 'rule 1000 http.get_args["id"] contains "cmd"  ;';
+        proxy_pass http://127.0.0.1:$TEST_NGINX_SERVER_PORT/echo;
+    }
     
-#     location /echo {
-#         return 200 "echo";
-#     }
-# --- request
-# GET /test_handler?id=aaaaacmd
-# --- error_log
-# Matched pattern: cmd
-# --- no_error_log
-# [error]
+    location /echo {
+        return 200 "echo";
+    }
+--- request
+GET /test_handler?id=aaaaacmd
+--- error_log
+could not detect url encoding param: id = aaaaacmd (no URL encoding)
+Matched pattern: cmd
+--- no_error_log
+[error]
